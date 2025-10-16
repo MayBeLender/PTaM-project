ee89c9b05750d705eddd0465f6f0b3300973c51c
MayBeLender <vladlaskin203@gmail.com>
2025-10-16 18:07:23 +0600

    lr2

--- python/main.py
+++ python/main.py
@@ -1,1 +1,19 @@
-print("test")
+length = 8
+
+a = str(input())
+if (len(a) > length) or not('1' in a) or not('0' in a): exit()
+a += '0' * (length - len(a))
+print(a)
+
+b = str(input())
+if (len(b) > length) or not('1' in b) or not('0' in b): exit()
+b += '0' * (length - len(b))
+print(b)
+
+result = ""
+for i in range(length):
+    if (a[i] == '1') & (b[i] == '1'):
+        result += '1'
+    else: result += '0'
+
+print(result)
